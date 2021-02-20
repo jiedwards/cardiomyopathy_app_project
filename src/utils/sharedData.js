bab6255d6b9d338c1d94838baa62fd2169cf2b08
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2021-02-20 11:25:45 +0000

    implemented shared data arrays, and updated the add form to reduce risk of user misinput

---
+++ src/utils/sharedData.js
@@ -0,0 +1,22 @@
+let cardiomyopathyTypes = [
+    "Dilated cardiomyopathy(DCM)",
+    "Hypertrophic cardiomyopathy(HCM)",
+    "Restrictive cardiomyopathy(RCM)",
+    "Left Ventricular Non - compaction(LVNC)",
+    "Arrhythmogenic Right Ventricular Dysplasia(ARVD)"
+]
+
+let mutatedGenes = [
+    "TNNT",
+    "MYH",
+    "TPM1",
+    "MYBPC3"
+]
+
+let chartDataTypes = [
+    "TEST1",
+    "TEST2"
+
+]
+
+export { cardiomyopathyTypes, mutatedGenes, chartDataTypes };
