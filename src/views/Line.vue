e4b995300758f2f120e996f91bf3b7d884600304
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2021-02-20 23:30:45 +0000

    refactored code to reduce load on DB due to excessive API calls, implemented local storage to cache firebase API response

--- src/views/Line.vue
+++ src/views/Line.vue
@@ -67,7 +67,7 @@
 
     <br /><br />
     <line-chart
-      :experimentalData="experimentalData"
+      :experimentalData="experimentalDataCharts"
       class="center"
     ></line-chart>
   </div>
@@ -82,7 +82,8 @@ export default {
   data() {
     return {
       welcomeMessage: ref(""),
-      experimentalData: ref([]),
+      experimentalDataCharts: ref([]),
+      firebaseApiExperimentalDataCache: ref([]),
       cardiomyopathyTypes: ref(new Set()),
       mutatedGenes: ref(new Set()),
       cardiomyopathy_type_choice: ref(""),
@@ -91,57 +92,12 @@ export default {
   },
   beforeMount() {
     this.GetExperimentalData();
-    this.GetExperimentalDataDropdownOptions();
   },
   methods: {
-    OnCardiomyopathyTypeDropdownChange(event) {
-      this.cardiomyopathy_type_choice = event.target.value;
-      this.GetExperimentalDataDropdownOptions();
-    },
-    OnMutatedGeneDropdownChange(event) {
-      this.mutated_gene_choice = event.target.value;
-    },
-    GetExperimentalDataDropdownOptions() {
-      this.mutatedGenes.clear();
-
-      firebaseDb
-        .collection("experimental-data")
-        .get()
-        .then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            let docData = doc.data();
-
-            if (typeof docData.cardiomyopathy_type !== "undefined") {
-              this.cardiomyopathyTypes.add(docData.cardiomyopathy_type);
-            }
-
-            if (
-              typeof docData.gene !== "undefined" &&
-              this.cardiomyopathy_type_choice == docData.cardiomyopathy_type
-            ) {
-              this.mutatedGenes.add(docData.gene);
-            }
-          });
-        });
-    },
-    GetFilteredExperimentalData() {
-      this.experimentalData = [];
-
-      var queryResults = firebaseDb
-        .collection("experimental-data")
-        .where("gene", "==", this.mutated_gene_choice)
-        .where("cardiomyopathy_type", "==", this.cardiomyopathy_type_choice);
-
-      // No edge case is needed to check the data exists as the dropdown is dynamically updated and only shows what is available
-      queryResults.get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          let docData = doc.data();
-
-          this.GenerateChart(docData);
-        });
-      });
-    },
     GetExperimentalData() {
+      // Delete local storage cache of API response before calling for updated data.
+      localStorage.removeItem("firebaseAPIResponseCache");
+
       firebaseDb
         .collection("experimental-data")
         .get()
@@ -157,77 +113,137 @@ export default {
                 docData.x_axis_tick_amount = docData.x_axis_data.length;
               }
 
-              this.GenerateChart(docData);
+              this.firebaseApiExperimentalDataCache.push(docData);
             });
+
+            // API response is cached to reduce firestore API calls.
+            localStorage.setItem(
+              "firebaseAPIResponseCache",
+              JSON.stringify(this.firebaseApiExperimentalDataCache)
+            );
+
+            this.GenerateCharts(this.firebaseApiExperimentalDataCache);
+            // Chart options must be populated after cache is saved in order to reduce API calls.
+            this.GetExperimentalDataDropdownOptions();
           }
         })
         .catch((error) => {
           console.log("Error getting documents: ", error);
         });
     },
-    GenerateChart(docData) {
-      let chart = {
-        chartOptions: {
-          chart: {
-            height: 350,
-            type: "line",
-            zoom: {
+    GetFilteredExperimentalData() {
+      this.firebaseApiExperimentalDataCache = [];
+
+      var firebaseAPIResponseCache = JSON.parse(
+        localStorage.getItem("firebaseAPIResponseCache")
+      );
+
+      // No edge case is needed to check the data exists as the dropdown is dynamically updated and only shows what is available
+      firebaseAPIResponseCache.forEach((docData) => {
+        if (
+          docData.gene == this.mutated_gene_choice &&
+          docData.cardiomyopathy_type == this.cardiomyopathy_type_choice
+        ) {
+          this.firebaseApiExperimentalDataCache.push(docData);
+        }
+      });
+      this.GenerateCharts(this.firebaseApiExperimentalDataCache);
+    },
+    GenerateCharts(firebaseApiExperimentalDataCache) {
+      this.experimentalDataCharts = [];
+
+      firebaseApiExperimentalDataCache.forEach((docData) => {
+        let chart = {
+          chartOptions: {
+            chart: {
+              height: 350,
+              type: "line",
+              zoom: {
+                enabled: false,
+              },
+            },
+            dataLabels: {
               enabled: false,
             },
-          },
-          dataLabels: {
-            enabled: false,
-          },
-          stroke: {
-            width: [5, 7, 5],
-            curve: "straight",
-            dashArray: [0, 8, 5],
-          },
-          title: {
-            text: docData.chart_title,
-            align: "left",
-          },
-          grid: {
-            row: {
-              colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-              opacity: 0.5,
+            stroke: {
+              width: [5, 7, 5],
+              curve: "straight",
+              dashArray: [0, 8, 5],
             },
-          },
-          xaxis: {
-            categories: docData.x_axis_data,
-            tickAmount: docData.x_axis_tick_amount - 1,
             title: {
-              text: docData.x_axis_label,
+              text: docData.chart_title,
+              align: "left",
+            },
+            grid: {
+              row: {
+                colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+                opacity: 0.5,
+              },
+            },
+            xaxis: {
+              categories: docData.x_axis_data,
+              tickAmount: docData.x_axis_tick_amount - 1,
+              title: {
+                text: docData.x_axis_label,
+              },
+            },
+            yaxis: {
+              title: {
+                text: docData.y_axis_label,
+              },
+            },
+            legend: {
+              position: "top",
+              horizontalAlign: "right",
+              floating: true,
+              offsetY: -25,
+              offsetX: -5,
             },
           },
-          yaxis: {
-            title: {
-              text: docData.y_axis_label,
+          series: [
+            {
+              name: docData.gene,
+              data: docData.y_axis_data,
             },
-          },
-          legend: {
-            position: "top",
-            horizontalAlign: "right",
-            floating: true,
-            offsetY: -25,
-            offsetX: -5,
-          },
-        },
-        series: [
-          {
-            name: docData.gene,
-            data: docData.y_axis_data,
-          },
-          //   {
-          //     name: "Laptops",
-          //     data: [20, 61, 75, 71, 69, 72, 89, 111, 168],
-          //   },
-        ],
-      };
+            //   {
+            //     name: "Laptops",
+            //     data: [20, 61, 75, 71, 69, 72, 89, 111, 168],
+            //   },
+          ],
+        };
 
-      this.welcomeMessage = "Welcome to the experimental data chart page.";
-      this.experimentalData.push({ docData, chart });
+        this.welcomeMessage = "Welcome to the experimental data chart page.";
+        this.experimentalDataCharts.push({ docData, chart });
+      });
     },
+    GetExperimentalDataDropdownOptions() {
+      this.mutatedGenes.clear();
+
+      var firebaseAPIResponseCache = JSON.parse(
+        localStorage.getItem("firebaseAPIResponseCache")
+      );
+
+      firebaseAPIResponseCache.forEach((docData) => {
+        if (typeof docData.cardiomyopathy_type !== "undefined") {
+          this.cardiomyopathyTypes.add(docData.cardiomyopathy_type);
+        }
+
+        if (
+          typeof docData.gene !== "undefined" &&
+          this.cardiomyopathy_type_choice == docData.cardiomyopathy_type
+        ) {
+          this.mutatedGenes.add(docData.gene);
+        }
+      });
+    },
+    OnCardiomyopathyTypeDropdownChange(event) {
+      this.cardiomyopathy_type_choice = event.target.value;
+      this.GetExperimentalDataDropdownOptions();
+    },
+    OnMutatedGeneDropdownChange(event) {
+      this.mutated_gene_choice = event.target.value;
+    },
+    
   },
   components: {
     LineChart,
